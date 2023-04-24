3f19dc89210c3941c7aae129babd5c1660d77fb4
Harish238 <118815836+Harish238@users.noreply.github.com>
2023-04-24 18:30:18 +0530

    Update source.js

--- source.js
+++ source.js
@@ -1,7 +1,11 @@
-function age(){
-    let d1 = document.getElementById("date").value;
-    let m1 = document.getElementById("month").value;
-    let y1 = document.getElementById("year").value;
+function age() {  
+    let d1 = document.getElementById('date').value;  
+    let m1 = document.getElementById('month').value;  
+    let y1 = document.getElementById('year').value;  
+    let date = new Date();  
+    let d2 = date.getDate();  
+    let m2 = 1 + date.getMonth();  
+    let y2 = date.getFullYear();  
     if(isNaN(d1)||d1<1||d1>31){
         alert("Please enter a valid date (1-31).");
         document.getElementById("date").focus();
@@ -17,23 +21,17 @@ function age(){
         document.getElementById("year").focus();
         return;
     }
-
-    let date = new Date();
-    let d2 = date.getDate();
-    let m2 = 1+date.getMonth();
-    let y2 = date.getFullYear();
-    let month = [31,28,31,30,31,30,31,31,30,31,30,31];
-    if(d1>d2){
-        d2 = d2+month[m2-1];
-        m2=m2-1;
-    }
-    if(m1>m2){
-        m2 = m2+1;
-        y2 = y2-1;
-    }
-    let d = d2-d1;
-    let m = m2-m1;
-    let y = y2-y1;
-    
-    document.getElementById("age").innerHTML = "Your Age is"+" "+ y +"Years"+" "+ m +"Months"+" "+ d +"Days";
-}
+    let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];  
+    if(d1 > d2){  
+     d2 = d2 + month[m2 - 1];  
+     m2 = m2 - 1;  
+    }  
+    if(m1 > m2){  
+     m2 = m2 + 12;  
+     y2 = y2 - 1;  
+    }  
+    let d = d2 - d1;  
+    let m = m2 - m1;  
+    let y = y2 - y1;  
+    document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';  
+   }  
